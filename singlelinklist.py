8abbd7164d2ac2564f2c70d5e6fa3f4bdf3add19
gssjl2008 <gssjl2004@126.com>
2019-12-26 10:44:59 +0800

    单链表增加查询和更新方法

--- singlelinklist.py
+++ singlelinklist.py
@@ -16,6 +16,8 @@ class Node:
     # def __init__(self, data, next):
     #     self.data = data
     #     self.next = next
+    def __str__(self):
+        return "data: %s, next:%s" %(self.data, self.next)
 
     # 设置 __repr__ 显示具体的值，方便查看
     def __repr__(self):
@@ -137,6 +139,89 @@ class Linklist:
                 cur = cur.next
             pre.next = None
 
+    def update(self, ord, new):
+        '''
+        更新链表
+        :param ord:  str or int  str则值查找替换， int则用索引来重新赋值
+        :param new:  新的值
+        :return:
+        '''
+        if self.is_empty():
+            print("Linklist is empty!")
+            return
+
+        if isinstance(ord, int):
+            n = 0
+            cur = self.root
+            while cur.next != None:
+                print('while loop ; %s' %n)
+                if n == ord:
+                    cur.data = new
+                    return
+                n += 1
+                cur = cur.next
+            print('while end ; %s' % n)
+            if n == ord:
+                cur.data = new
+            else:
+                raise IndexError("Linklist index error!")
+
+        elif isinstance(ord, str):
+            cur = self.root
+            while cur.next != None:
+                print('while loop ; %s' % cur)
+                if cur.data == ord:
+                    cur.data = new
+                    return
+                cur = cur.next
+            print('while end ; %s' % cur)
+            if cur.data == ord:
+                cur.data = new
+            else:
+                raise KeyError('Linklist has not key %s' %ord)
+        else:
+            raise TypeError("ord must be str or int")
+
+    def get(self, key):
+        if self.is_empty():
+            print("Linklist is empty!")
+            return
+
+        if isinstance(key, int):
+            n = 0
+            cur = self.root
+            while cur.next != None:
+                print('while loop ; %s' %n)
+                if n == key:
+                    print(cur)
+                    return cur
+                n += 1
+                cur = cur.next
+            print('while end ; %s' % n)
+            if n == key:
+                print(cur)
+                return cur
+            else:
+                raise IndexError("Linklist index error!")
+
+        elif isinstance(key, str):
+            cur = self.root
+            while cur.next != None:
+                print('while loop ; %s' % cur)
+                if cur.data == key:
+                    print(cur)
+                    return cur
+                cur = cur.next
+            print('while end ; %s' % cur)
+            if cur.data == key:
+                print(cur)
+                return cur
+            else:
+                raise KeyError('Linklist has not key %s' %ord)
+        else:
+            raise TypeError("ord must be str or int")
+
+
     def all(self):
         '''
         用列表和字典的形式展示链表
@@ -167,6 +252,9 @@ if __name__ == '__main__':
     s.remove('abc')
     s.all()
     s.add('bcd')
+    s.all()
+    s.update(0, 'yyy')
+    s.all()
     s.add('cde', head=True)
     s.add('def')
     s.add('efg', head=True)
@@ -178,4 +266,11 @@ if __name__ == '__main__':
     s.all()
     s.remove('efg')
     s.all()
-
+    s.update(2, 'xxx')
+    s.all()
+    s.update('xxx', 'xyz')
+    s.all()
+    s.get('xyz')
+    print("---"*20)
+    for i in range(s.size()):
+        s.get(i)
